72604c935440e667ac6295d48667f9706f1c8854
MolodduwaGabriel <molodduwa.quiroz.gabriel.andres@iisgalvanimi.edu.it>
2019-10-16 09:35:30 +0000

    finito

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client-angular';
+export class AppComponent implements OnInit{
+  results: string[];
+  private BASE_URL:string = '/api';
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+
+      // Make the HTTP request:
+      this.http.get('https://3000-cd6fe4de-4a24-4ad5-978c-c50927d2945c.ws-eu0.gitpod.io/api').subscribe(data => {
+        // Read the result field from the JSON response.
+        this.results = data['results'];
+      });
+    }
 }
